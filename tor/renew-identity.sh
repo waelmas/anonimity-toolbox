f45f1534b7a33acdbf9f981c0366016b43bc277a
Wael Al Masri <43114112+waelmas@users.noreply.github.com>
2022-03-01 22:06:48 +0200

    minor fixes and git ignoring perms

--- tor/renew-identity.sh
+++ tor/renew-identity.sh
@@ -2,6 +2,5 @@
 source torsocks off
 printf "\nProvide the Tor control-port password:\n"
 read torpassraw
-torpassquoted = "${torpassraw}"
-sudo echo -e "AUTHENTICATE ${torpassraw}\r\nSIAGNAL NEWNYM\r\nSIGNAL CLEARDNSCACHE\r\nQUIT" | sudo nc 127.0.0.1 9051
+sudo echo -e "AUTHENTICATE "${torpassraw}"\r\nSIAGNAL NEWNYM\r\nSIGNAL CLEARDNSCACHE\r\nQUIT" | sudo nc 127.0.0.1 9051
 source torsocks on
